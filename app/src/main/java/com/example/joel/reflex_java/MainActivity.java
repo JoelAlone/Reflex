b9ec6605aae9ffcbca14c04b0d7d43ba291eaf4a
Questionidas <39231348+Questionidas@users.noreply.github.com>
2018-06-05 22:03:34 +0200

    Adding "best score" functionality

--- app/src/main/java/com/example/joel/reflex_java/MainActivity.java
+++ app/src/main/java/com/example/joel/reflex_java/MainActivity.java
@@ -6,12 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button b_main, b_start;
 
-    long startTime, endTime, currentTime;
+    TextView tv_info;
+
+    long startTime, endTime, currentTime, bestTime = 10000;
 
 
     @Override
@@ -23,13 +26,18 @@ public class MainActivity extends AppCompatActivity {
         b_start = (Button) findViewById(R.id.b_start);
         b_main = (Button) findViewById(R.id.b_main);
 
+        tv_info = (TextView) findViewById(R.id.tv_info);
+
         b_start.setEnabled(true);
         b_main.setEnabled(false);
+
+        tv_info.setText("BEST RESULT: " + bestTime + " ms");
 
         b_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 b_start.setEnabled(false);
+                b_main.setText("");
 
                 Handler handler = new Handler();
                 handler.postDelayed(new Runnable() {
@@ -56,7 +64,13 @@ public class MainActivity extends AppCompatActivity {
                 b_main.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.red));
                 b_main.setText(currentTime + "ms");
                 b_start.setEnabled(true);
-                b_main.setEnabled(false);
+
+
+                if (currentTime < bestTime) {
+                    bestTime = currentTime;
+                    tv_info.setText("BEST RESULT: " + bestTime + " ms");
+                }
+
             }
         });
 
@@ -68,3 +82,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+

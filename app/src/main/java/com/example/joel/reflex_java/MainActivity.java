a6062c33056e4f0984ca3f26e1a5c3a6d6bdc6ed
michalmendala <39330670+michalmendala@users.noreply.github.com>
2018-06-06 02:46:18 +0200

    Aktualizacja MainActivity.java

--- app/src/main/java/com/example/joel/reflex_java/MainActivity.java
+++ app/src/main/java/com/example/joel/reflex_java/MainActivity.java
@@ -6,12 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 
 public class MainActivity extends AppCompatActivity {
 
     Button b_main, b_start;
 
-    long startTime, endTime, currentTime;
+    TextView tv_info;
+
+    long startTime, endTime, currentTime, bestTime = 10000;
 
 
     @Override
@@ -23,13 +29,20 @@ public class MainActivity extends AppCompatActivity {
         b_start = (Button) findViewById(R.id.b_start);
         b_main = (Button) findViewById(R.id.b_main);
 
+        tv_info = (TextView) findViewById(R.id.tv_info);
+
         b_start.setEnabled(true);
         b_main.setEnabled(false);
+
+        tv_info.setText("BEST RESULT: " + bestTime + " ms");
 
         b_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 b_start.setEnabled(false);
+                b_main.setText("");
+
+                int random = ThreadLocalRandom.current().nextInt(1000, 5000 + 1);
 
                 Handler handler = new Handler();
                 handler.postDelayed(new Runnable() {
@@ -44,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
 
                     }
-                }, 2000);
+                }, random);
             }
         });
 
@@ -56,7 +69,13 @@ public class MainActivity extends AppCompatActivity {
                 b_main.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.red));
                 b_main.setText(currentTime + "ms");
                 b_start.setEnabled(true);
-                b_main.setEnabled(false);
+
+
+                if (currentTime < bestTime) {
+                    bestTime = currentTime;
+                    tv_info.setText("BEST RESULT: " + bestTime + " ms");
+                }
+
             }
         });
 
